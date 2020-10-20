c94ed456052700a611fcd9738f83c15cbc52b28c
Jogi Prasad Pakki <p.j.prasad02@gmail.com>
2020-10-20 20:06:40 +0530

    first commit

---
+++ AndroidNativeCore/AndroidFileManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AndroidNativeCore
+{
+    public class AndroidFileManager
+    {
+        AndroidJavaObject a;
+        public AndroidFileManager()
+        {
+           a = new AndroidJavaObject(AndroidCore.PluginPackage + ".Core");
+        }
+        public bool makeDirectory(string path)
+        {          
+           return a.Call<bool>("createDirectory", path);
+        }
+        public void writeFile(string path,string child,byte[] data)
+        {
+            a.Call("writeFile", path,child,data);
+        }
+        public bool deleteFile(string path)
+        {
+            return a.Call<bool>("deleteFile", path);
+        }
+        public byte[] readFile(string path)
+        {
+           return a.Call<byte[]>("readFile", path);
+        }
+        public bool isFileExits(string path)
+        {
+            return a.Call<bool>("isFileExits", path);
+        }
+    }
+}
